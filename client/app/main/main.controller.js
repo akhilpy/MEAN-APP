b57335aee8843b1955e9e00cb807af8f96bb11f8
Jill Molloy <hello@jillmolloy.com>
2016-01-27 18:53:11 -0800

    add content editing

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,12 +1,22 @@
 'use strict';
 
+(function() {
+
 class MainController {
   image = 0;
 
-  constructor() {
-
+  constructor($scope) {
+    $scope.hero = {
+      title: "It’s borrowing reinvented. It’s investing on your terms."
+    }
+    $scope.editMode = false;
+    $scope.toggleEditMode = function() {
+      $scope.editMode = $scope.editMode === false ? true: false;
+    }
   }
 }
 
 angular.module('investnextdoorCaApp')
   .controller('MainController', MainController);
+
+})();
