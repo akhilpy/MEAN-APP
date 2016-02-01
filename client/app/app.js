5cb56d17ae3d48069dd3aae1877c0e68662155ea
Jill Molloy <hello@jillmolloy.com>
2016-02-01 14:27:11 -0800

    updates

--- client/app/app.js
+++ client/app/app.js
@@ -10,11 +10,20 @@ angular.module('investnextdoorCaApp', [
   'btford.socket-io',
   'ui.router',
   'validation.match',
-  'textAngular'
+  'textAngular',
+  'formly',
+  'formlyBootstrap'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, formlyConfigProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    formlyConfigProvider.setType({
+      name: 'chosen',
+      extends: 'select',
+      template: '<select chosen="model[options.key]" data-placeholder="Select" class="form-control" disable-search-threshold="15" ng-model="model[options.key]"></select>'
+    });
+
   });
