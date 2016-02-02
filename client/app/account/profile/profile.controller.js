00513d68fdfe99be556a855022a258078f70ad42
Jill Molloy <hello@jillmolloy.com>
2016-02-01 17:26:01 -0800

    wiring investor profile

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class ProfileController {
-  constructor($http, Auth, $state, appConfig) {
+  constructor($http, Auth, $state, appConfig, Form) {
     this.$http = $http;
     this.$state = $state;
     this.errors = {};
@@ -15,6 +15,11 @@ class ProfileController {
     this.isBorrower = Auth.isBorrower;
     this.isInvestor = Auth.isInvestor;
     this.user = Auth.getCurrentUser();
+
+    this.Form = Form;
+
+    this.investorProfile = this.Form.getInvestorProfile();
+    this.borrowerProfile = this.Form.getBorrowerProfile();
   }
 
   changePassword(form) {
@@ -34,7 +39,27 @@ class ProfileController {
   }
 
   updateInvestor(form) {
+    var address = {};
+    var investor = {};
+
     this.submitted = true;
+
+    if( this.user.address ) {
+      address = {
+        street: this.user.address.street,
+        city: this.user.address.city,
+        province: this.user.address.province,
+        postal: this.user.address.postal
+      };
+    }
+
+    if( this.user.investor ) {
+      investor = {
+        increase: this.user.investor.increase,
+        notifications: this.user.investor.notifications,
+        notes: this.user.investor.notes
+      };
+    }
 
     if (form.$valid) {
       this.$http.put('/api/users/' + this.user._id, {
@@ -43,7 +68,12 @@ class ProfileController {
         name: {
           first: this.user.name.first,
           last: this.user.name.last
-        }
+        },
+        phone: this.user.phone,
+        social: this.user.social,
+        dob: this.user.dob,
+        address: address,
+        investor: investor
       })
       .then(() => {
         this.$state.go('dashboard.index');
--- client/components/formFields/formFields.service.js
+++ client/components/formFields/formFields.service.js
@@ -4,6 +4,7 @@
 
 function FormService(appConfig) {
   var roles = appConfig.ROLES;
+  var provinces = appConfig.PROVINCES;
 
   var Form = {
 
@@ -138,7 +139,285 @@ function FormService(appConfig) {
         }
       ];
 
-    }
+    },
+
+
+    /**
+     * Get Investor Profile Fields
+     *
+     * @return Array
+     */
+    getInvestorProfile() {
+      return [
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'username',
+              templateOptions: {
+                label: 'Username',
+                placeholder: 'username',
+                description: 'This name will appear to other users of the site (i.e. joe123, investor111)'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'name.first',
+              templateOptions: {
+                label: 'First Name',
+                placeholder: 'First'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'name.last',
+              templateOptions: {
+                label: 'Last Name',
+                placeholder: 'Last'
+              },
+              expressionProperties: {
+                'templateOptions.disabled': '!model.name.first'
+              }
+            }
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'email',
+              templateOptions: {
+                type: 'email',
+                label: 'Email Address',
+                placeholder: 'name@host.com'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'phone',
+              templateOptions: {
+                label: 'Phone',
+                placeholder: '###-###-####'
+              }
+            },
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'dob',
+              templateOptions: {
+                type: 'date',
+                label: 'Date of Birth',
+                placeholder: 'mm-dd-yyyy',
+                description: 'We need your birth date for validation purposes.'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'social',
+              templateOptions: {
+                label: 'Social Security Number',
+                placeholder: '###',
+                description: 'Please provide the last 3 digits of your social security number, this is required for verification purposes.'
+              }
+            }
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-2',
+              type: 'input',
+              key: 'address.street',
+              templateOptions: {
+                label: 'Street Address',
+                placeholder: ''
+              }
+            },
+            {
+              className: 'flex-2',
+              type: 'input',
+              key: 'address.city',
+              templateOptions: {
+                label: 'City',
+                placeholder: ''
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'chosen',
+              key: 'address.province',
+              templateOptions: {
+                label: 'Province',
+                labelProp: 'label',
+                valueProp: 'value',
+                options: provinces,
+                ngOptions: 'option.name for option in to.options track by option.value',
+                placeholder: 'Select'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'address.postal',
+              templateOptions: {
+                label: 'Postal Code',
+                placeholder: ''
+              }
+            }
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'investor.increase',
+              templateOptions: {
+                type: 'checkbox',
+                label: 'Request increase to $1500 per offer limit.',
+                description: 'Due to securities regulations if you wish to invest more than $1500 in each listing you will need to apply for the appropriate investor status. Please indicate your interest and we will contact you shortly.'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'investor.notifications',
+              templateOptions: {
+                type: 'checkbox',
+                label: 'I wish to be notified when there are new listings.',
+                description: 'Opt in to be notified of new listings so that you do not miss out.'
+              }
+            },
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'textarea',
+              key: 'investor.notes',
+              templateOptions: {
+                label: 'Request',
+                description: 'Let us know if there is a type of investment that you are specifically looking for.'
+              }
+            }
+          ]
+        }
+      ];
+
+    },
+
+
+    /**
+     * Get Borrower Profile Fields
+     *
+     * @return Array
+     */
+    getBorrowerProfile() {
+      return [
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'username',
+              templateOptions: {
+                label: 'Username',
+                placeholder: 'username'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'name.first',
+              templateOptions: {
+                label: 'First Name',
+                placeholder: 'First'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'name.last',
+              templateOptions: {
+                label: 'Last Name',
+                placeholder: 'Last'
+              },
+              expressionProperties: {
+                'templateOptions.disabled': '!model.name.first'
+              }
+            }
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'email',
+              templateOptions: {
+                type: 'email',
+                label: 'Email Address',
+                placeholder: 'name@host.com'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'phone',
+              templateOptions: {
+                label: 'Phone',
+                placeholder: '###-###-####'
+              }
+            },
+          ]
+        },
+        {
+          className: 'display-flex',
+          fieldGroup: [
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'password',
+              templateOptions: {
+                type: 'password',
+                label: 'Password',
+                placeholder: 'Password'
+              }
+            },
+            {
+              className: 'flex-1',
+              type: 'input',
+              key: 'confirmPassword',
+              templateOptions: {
+                type: 'password',
+                label: 'Confirm Password',
+                placeholder: 'Password'
+              }
+            }
+          ]
+        }
+      ];
+
+    },
 
   };
 
